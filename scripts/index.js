dc1b83994baca936bd361bcee9a3fe66f2909e73
Sergey Lozin <lozinizol@yahoo.com>
2025-03-14 20:28:21 +0300

    versionOne

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
-
 // @todo: Функция создания карточки
-
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+// Добавляем функцию для удаления карточки
+
+function createCard(cardData, deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const card = cardTemplate.cloneNode(true);
+
+  card.querySelector(".card__image").src = cardData.link;
+  card.querySelector(".card__title").textContent = cardData.name;
+
+  card
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
+
+  document.querySelector(".places__list").appendChild(card);
+
+  return card;
+}
+
+function deleteCard(event) {
+  const card = event.target.closest(".card");
+  card.remove();
+}
+
+initialCards.forEach(function (cardData) {
+  createCard(cardData, deleteCard);
+});
