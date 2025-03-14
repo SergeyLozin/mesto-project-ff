bcb704b49d03708a2161cc7f48aaba319e10a7e1
Sergey Lozin <lozinizol@yahoo.com>
2025-03-14 20:29:21 +0300

    versionOneUpdated

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,4 @@
-// @todo: Темплейт карточки
-// @todo: DOM узлы
-// @todo: Функция создания карточки
-// @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
-
-// Добавляем функцию для удаления карточки
 
 function createCard(cardData, deleteCard) {
   const cardTemplate = document.querySelector("#card-template").content;
